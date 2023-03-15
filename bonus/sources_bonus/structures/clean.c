8e851dba20b498ec705aa134f29dbc89efbd229f
Thea Wang <theawang@yahoo.com>
2023-03-15 10:31:28 +0100

    all leaks cleared

--- bonus/sources_bonus/structures/clean.c
+++ bonus/sources_bonus/structures/clean.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   clean.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wangthea <wangthea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 19:13:34 by wangthea          #+#    #+#             */
-/*   Updated: 2023/03/13 18:25:46 by wangthea         ###   ########.fr       */
+/*   Updated: 2023/03/15 10:28:38 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	clean_map(t_game *g)
 {
 	ft_free((void **) g->map.map, g->map.size.y);
 	ft_free((void **) g->map.map_tmp, g->map.size.y);
+	free(g->slime.pos);
 	exit(1);
 }
 
@@ -26,7 +27,7 @@ int	clean(t_game *g)
 	clean_textures(g);
 	if (g->window)
 		mlx_destroy_window(g->mlx, g->window);
-	// mlx_destroy_display(g->mlx);
+	mlx_destroy_display(g->mlx);
 	free(g->mlx);
 	exit(0);
 }
